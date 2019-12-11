7151b60605da9dc013ddd1c609875cb7ef620a3f
Yanbo <yanbo@YanbodeMacBook-Pro.local>
2019-12-11 10:20:15 +0800

    方法定义

--- src/ElasticsearchEngine.php
+++ src/ElasticsearchEngine.php
@@ -8,4 +8,48 @@ use Laravel\Scout\Engines\Engine;
 class ElasticsearchEngine extends Engine
 {
 
+	public function __construct()
+	{
+		
+	}
+
+	public function update($models)
+	{
+
+	}
+
+	public function delete($models)
+	{
+
+	}
+
+	public function search(Builder $builder)
+	{
+
+	}
+
+	public function paginate(Builder $builder, $perPage, $page)
+	{
+
+	}
+
+	public function mapIds($results)
+	{
+
+	}
+
+	public function map($results, $model)
+	{
+
+	}
+
+	public function getTotalCount($results)
+	{
+
+	}
+
+	public function flush($model)
+	{
+
+	}
 }
